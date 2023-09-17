dd0c938928ead1fd2a1cbf91d16869cf6c24ccc5
Roberto Jacobo <robjacobox@gmail.com>
2023-09-17 00:43:39 -0600

    pipes app done

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
-    
+
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { FieldsetModule } from "primeng/fieldset";
 import { MenubarModule } from "primeng/menubar";
 import { PanelModule } from "primeng/panel";
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from "primeng/toolbar";
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
@@ -15,6 +18,9 @@ import { PanelModule } from "primeng/panel";
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    SplitButtonModule,
+    ToolbarModule,
+    TableModule
   ],
 })
 export class PrimeNgModule {}
